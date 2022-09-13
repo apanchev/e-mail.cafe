5bb1f8fb4a3cfa342b01d036dc7922269845a16c
apanchev <22640082+apanchev@users.noreply.github.com>
2022-09-13 22:25:27 +0200

    feat(terraform): start aws configuration w/ terraform

---
+++ terraform/providers.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>4.30.0"
+    }
+  }
+
+  backend "s3" {
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.region
+
+  default_tags {
+    tags = {
+      Region     = var.region
+      Project    = var.project
+      Env        = var.env
+      Zone       = local.zone
+      DeployedBy = "Terraform"
+    }
+  }
+}
